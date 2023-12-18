3968810608a714222fcf2cb265779dd57717e2f3
wogikaze <kouse2319@gmail.com>
2023-12-19 00:25:21 +0900

    ランダムバトルの実装

---
+++ python-ai/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+max_damage_player
--- python-ai/test1.py
+++ python-ai/test1.py
@@ -18,132 +18,23 @@ class MaxDamagePlayer(Player):
 
 
 async def main():
-    team_1 = """
-Goodra (M) @ Assault Vest
-Ability: Sap Sipper
-EVs: 248 HP / 252 SpA / 8 Spe
-Modest Nature
-IVs: 0 Atk
-- Dragon Pulse
-- Flamethrower
-- Sludge Wave
-- Thunderbolt
-
-Sylveon (M) @ Leftovers
-Ability: Pixilate
-EVs: 248 HP / 244 Def / 16 SpD
-Calm Nature
-IVs: 0 Atk
-- Hyper Voice
-- Mystical Fire
-- Protect
-- Wish
-
-Cinderace (M) @ Life Orb
-Ability: Blaze
-EVs: 252 Atk / 4 SpD / 252 Spe
-Jolly Nature
-- Pyro Ball
-- Sucker Punch
-- U-turn
-- High Jump Kick
-
-Toxtricity (M) @ Throat Spray
-Ability: Punk Rock
-EVs: 4 Atk / 252 SpA / 252 Spe
-Rash Nature
-- Overdrive
-- Boomburst
-- Shift Gear
-- Fire Punch
-
-Seismitoad (M) @ Leftovers
-Ability: Water Absorb
-EVs: 252 HP / 252 Def / 4 SpD
-Relaxed Nature
-- Stealth Rock
-- Scald
-- Earthquake
-- Toxic
-
-Corviknight (M) @ Leftovers
-Ability: Pressure
-EVs: 248 HP / 80 SpD / 180 Spe
-Impish Nature
-- Defog
-- Brave Bird
-- Roost
-- U-turn
-"""
-    team_2 = """
-Togekiss @ Leftovers
-Ability: Serene Grace
-EVs: 248 HP / 8 SpA / 252 Spe
-Timid Nature
-IVs: 0 Atk
-- Air Slash
-- Nasty Plot
-- Substitute
-- Thunder Wave
-
-Galvantula @ Focus Sash
-Ability: Compound Eyes
-EVs: 252 SpA / 4 SpD / 252 Spe
-Timid Nature
-IVs: 0 Atk
-- Sticky Web
-- Thunder Wave
-- Thunder
-- Energy Ball
-
-Cloyster @ Assault Vest
-Ability: Skill Link
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Icicle Spear
-- Rock Blast
-- Ice Shard
-- Shell Smash
-
-Sandaconda @ Focus Sash
-Ability: Sand Spit
-EVs: 252 Atk / 4 SpD / 252 Spe
-Jolly Nature
-- Stealth Rock
-- Glare
-- Earthquake
-- Rock Tomb
-
-Excadrill @ Focus Sash
-Ability: Sand Rush
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Iron Head
-- Rock Slide
-- Earthquake
-- Rapid Spin
-
-Cinccino @ Leftovers
-Ability: Skill Link
-EVs: 252 Atk / 4 Def / 252 Spe
-Jolly Nature
-- Bullet Seed
-- Knock Off
-- Rock Blast
-- Tail Slap
-"""
-
     # We create two players.
+    team1 = f"""
+
+"""
+    
     random_player = RandomPlayer(
-        battle_format="gen8ubers", team=team_1, max_concurrent_battles=10
+        battle_format="[Gen 9] Battle Stadium Singles Regulation E",
+        max_concurrent_battles=10,
     )
 
     max_damage_player = MaxDamagePlayer(
-        battle_format="gen8ubers", team=team_2, max_concurrent_battles=10
+        battle_format="[Gen 9] Battle Stadium Singles Regulation E",
+        max_concurrent_battles=10,
     )
 
     # Now, let's evaluate our player
-    await max_damage_player.battle_against(random_player, n_battles=10000)
+    await max_damage_player.battle_against(random_player, n_battles=100)
 
     print("Max damage player won %d / 100 battles" % max_damage_player.n_won_battles)
 
