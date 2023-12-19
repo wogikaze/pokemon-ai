9723eb75f8059722f7fdb3c064d979c3bf5c46b3
wogikaze <kouse2319@gmail.com>
2023-12-19 15:41:04 +0900

    人と対戦できるように

--- python-ai/test1.py
+++ python-ai/test1.py
@@ -20,23 +20,78 @@ class MaxDamagePlayer(Player):
 async def main():
     # We create two players.
     team1 = f"""
+dragonite @ lumberry
+Ability: multiscale
+EVs: 228 HP / 252 Atk / 28 Spe
+adamant Nature
+- extremespeed
+- earthquake
+- dragondance
+- encore
 
+fluttermane @ choicespecs
+Ability: protosynthesis
+EVs: 4 HP / 252 SpA / 252 Spe
+modest Nature
+- moonblast
+- shadowball
+- powergem
+- mysticalfire
+
+ogerponhearthflame @ hearthflamemask
+Ability: moldbreaker
+EVs: 252 HP / 4 Atk / 188 Def / 4 SpD / 60 Spe
+jolly Nature
+- ivycudgel
+- hornleech
+- encore
+- synthesis
+
+ursalunabloodmoon @ silkscarf
+Ability: mindseye
+EVs: 212 HP / 4 Def / 196 SpA / 4 SpD / 92 Spe
+modest Nature
+- bloodmoon
+- earthpower
+- hypervoice
+- yawn
+
+chienpao @ focussash
+Ability: swordofruin
+EVs: 252 Atk / 4 SpD / 252 Spe
+jolly Nature
+- iciclecrash
+- crunch
+- iceshard
+- sacredsword
+
+gholdengo @ covertcloak
+Ability: goodasgold
+EVs: 228 HP / 196 SpD / 84 Spe
+bold Nature
+- makeitrain
+- thunderwave
+- hex
+- recover
 """
-    
-    random_player = RandomPlayer(
-        battle_format="[Gen 9] Battle Stadium Singles Regulation E",
+
+    # random_player = RandomPlayer(
+    #     max_concurrent_battles=10,
+    # )
+
+    # max_damage_player = MaxDamagePlayer(
+    #     max_concurrent_battles=10,
+    # )
+    # # Now, let's evaluate our player
+    # await max_damage_player.battle_against(random_player, n_battles=100)
+    # print("Max damage player won %d / 100 battles" % max_damage_player.n_won_battles)
+
+    random_player = MaxDamagePlayer(
+        # battle_format="[Gen 9] Battle Stadium Singles Regulation E",
         max_concurrent_battles=10,
+        # team=team1,
     )
-
-    max_damage_player = MaxDamagePlayer(
-        battle_format="[Gen 9] Battle Stadium Singles Regulation E",
-        max_concurrent_battles=10,
-    )
-
-    # Now, let's evaluate our player
-    await max_damage_player.battle_against(random_player, n_battles=100)
-
-    print("Max damage player won %d / 100 battles" % max_damage_player.n_won_battles)
+    await random_player.send_challenges("wogikaze", n_challenges=1)
 
 
 if __name__ == "__main__":
