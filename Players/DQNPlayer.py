c97ac54bdbb1311d74cfcae446d143dcb3b69c53
wogikaze <kouse2319@gmail.com>
2024-01-15 20:38:01 +0900

    update dqnplayer

--- Players/DQNPlayer.py
+++ Players/DQNPlayer.py
@@ -1,12 +1,4 @@
-import asyncio
 from datetime import datetime
-from threading import Thread
-from typing import Dict
-import numpy as np
-import gymnasium as gym
-from gymnasium.spaces import Space, Box
-from gymnasium.utils.env_checker import check_env
-from poke_env import AccountConfiguration, LocalhostServerConfiguration, to_id_str
 from poke_env.player import RandomPlayer
 from poke_env.environment import AbstractBattle as Battle
 from poke_env.player.openai_api import ObsType, OpenAIGymEnv
@@ -29,52 +21,28 @@ from poke_env.player import (
 from tabulate import tabulate
 from wandb.integration.sb3 import WandbCallback
 from RLPlayer import RLPlayer
+import random
 
-if __name__ == "__main__":
-    """毎回idを変えておく()"""
-    # run_id は"lerning方法"+Date+Timeをコードにする
-    run_id = "DQN" + datetime.now().strftime("%d-%H-%M")
-    opponent = MaxDamagePlayer(battle_format="gen9randombattle")
-    train_env = RLPlayer(
-        battle_format="gen9randombattle", opponent=opponent, start_challenging=True
-    )
+
+class DQNRLPlayer(RLPlayer):
+    def get_opponent(self):
+        return [
+            RandomPlayer(battle_format="gen9randombattle"),
+            MaxDamagePlayer(battle_format="gen9randombattle"),
+            MaxDamagePlayer(battle_format="gen9randombattle"),
+            MaxDamagePlayer(battle_format="gen9randombattle"),
+            SimpleHeuristicsPlayer(battle_format="gen9randombattle"),
+        ]
+
+
+def evalute(run_id):
     opponent = RandomPlayer(battle_format="gen9randombattle")
     eval_env = RLPlayer(
         battle_format="gen9randombattle", opponent=opponent, start_challenging=True
     )
-    train_env = Monitor(train_env)
-    env = DummyVecEnv([lambda: train_env])
-
-    model = DQN(
-        "MlpPolicy",
-        env,
-        learning_rate=0.00025,
-        gamma=0.5,
-        target_update_interval=1,
-        verbose=1,
+    model = QRDQN.load(
+        "C:/Users/Wogikaze/Desktop/school/未来/models/mmt29uxf/model.zip", eval_env
     )
-    wandb.init(
-        project="sb3",
-        config=None,
-        sync_tensorboard=True,  # sb3のtensorboardメトリクスを自動アップロード
-        # monitor_gym=True,  # ゲームをプレイするエージェントの動画を自動アップロード
-        # save_code=True,  # 任意
-    )
-
-    model.learn(
-        total_timesteps=100000,
-        callback=WandbCallback(
-            model_save_path=f"models/{run_id}",
-            verbose=2,
-        ),
-        progress_bar=True,
-    )
-
-    env.close()
-
-    model.save(run_id)
-
-    # Evaluating the model
     print("Results against random player:")
 
     for step in range(100):
@@ -91,8 +59,57 @@ if __name__ == "__main__":
     print(
         f"{run_id} Evaluation: {eval_env.n_won_battles} victories out of {eval_env.n_finished_battles} episodes"
     )
+
+
+def train(run_id):
+    opponent = MaxDamagePlayer(account_configuration=None)
+    train_env = DQNRLPlayer(
+        battle_format="gen9randombattle", opponent=opponent, start_challenging=True
+    )
+    train_env = Monitor(train_env)
+    env = DummyVecEnv([lambda: train_env])
+
+    run = wandb.init(
+        project="sb3",
+        config=None,
+        sync_tensorboard=True,  # sb3のtensorboardメトリクスを自動アップロード
+        monitor_gym=True,  # ゲームをプレイするエージェントの動画を自動アップロード
+        save_code=True,  # 任意
+    )
+    model = QRDQN(
+        "MlpPolicy",
+        env,
+        learning_rate=0.00025,
+        gamma=0.5,
+        target_update_interval=1,
+        train_freq=(1, "step"),
+        verbose=1,
+        # tensorboard_log=f"runs/{run.id}",
+    )
+
+    model.learn(
+        total_timesteps=100_000,
+        callback=WandbCallback(
+            model_save_path=f"models/{run_id}",
+            model_save_freq=10000,
+            verbose=2,
+            gradient_save_freq=1000,
+        ),
+        progress_bar=True,
+        log_interval=1000,
+    )
+    run.finish()
     env.close()
 
+    model.save(f"models/{run_id}")
 
-def evaluate():
-    print("Evaluating...")
+
+if __name__ == "__main__":
+    """毎回idを変えておく()"""
+    # run_id は"lerning方法"+Date+Timeをコードにする
+    run_id = "QRDQN" + datetime.now().strftime("%d-%H-%M")
+    # run_id = "cpbk46g4"
+    # evalute(run_id)
+
+    train(run_id)
+    evalute(run_id)
