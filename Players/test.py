bd8aca7fed794ea05c6aff7e0063b4da0bd025a4
wogikaze <kouse2319@gmail.com>
2024-01-25 19:03:15 +0900

    add test.py

--- Players/test.py
+++ Players/test.py
@@ -1,8 +1,15 @@
-import gymnasium as gym
+import os
+
 import numpy as np
+
+import srl
+from srl.algorithms import dqn
+from srl.utils import common
+
+common.logger_print()
 from RLPlayer import RLPlayer
 from poke_env.player import RandomPlayer
-
+import gymnasium as gym
 import gymnasium.envs.registration
 
 gymnasium.envs.registration.register(
@@ -12,42 +19,50 @@ gymnasium.envs.registration.register(
 )
 env = gym.make("RLPlayer-v0", opponent=RandomPlayer())
 
-observation = env.reset()
 
-# for _ in range(10):
-#     observation, reward, terminated, truncated, info = env.step(
-#         env.action_space.sample()
-#     )
-#     env.render()
+def main():
+    env_config = srl.EnvConfig(
+        "RLPlayer-v0",
+        dict(opponent=RandomPlayer()),
+        gymnasium_make_func=gym.make,
+    )
 
-#     if terminated or truncated:
-#         observation = env.reset()
-#     print(observation)
+    # atariのパラメータでは大げさすきるので Pendulum 用に縮小
+    # あえて書いていますが、フレームワークのデフォルトは大体この値です。（詳細はコードを参照）
+    rl_config = dqn.Config()
+    rl_config.set_atari_config()
+    rl_config.hidden_block.set_mlp((64, 64))  # 画像処理層がないので隠れ層を増やしてます
+    rl_config.memory.capacity = 100_000  # 下げないとメモリ足りなくなる可能性あり
+    rl_config.window_length = 1  # Pendulum は加速も状態に含まれているので不要
+    rl_config.lr.set_constant(0.001)
+    rl_config.epsilon.set_constant(0.1)
+    rl_config.memory.warmup_size = 1000  # そこまで待たずに学習開始
+    rl_config.target_model_update_interval = 1000  # 大きすぎると学習がゆっくり
+    rl_config.enable_reward_clip = False  # 報酬clipしない
 
-# env.close()
+    # frameworkを指定したい場合 tensorflow or torch
+    # rl_config.framework.set_tensorflow()
 
-import numpy as np
+    runner = srl.Runner(env_config, rl_config)
+    runner.model_summary(expand_nested=True)
 
-import srl
-from srl.algorithms import ql
+    # --- train
+    state = runner.train(
+        max_episodes=10,
+    )
 
-env_config = srl.EnvConfig(
-    "RLPlayer-v0",
-    dict(opponent=RandomPlayer()),
-    gymnasium_make_func=gym.make,
-)
-# Q学習
-runner = srl.Runner(env_config, rl_config=ql.Config())
+    # --- evaluate
+    rewards = runner.evaluate(max_episodes=20)
+    print(f"Average reward for 20 episodes: {np.mean(rewards)}")
 
-# 学習
-runner.train(timeout=10)
 
-# 評価
-rewards = runner.evaluate(max_episodes=100)
-print("100エピソードの平均結果", np.mean(rewards))
+    from matplotlib import pyplot as plt
+    # x=np.linspace(0)
+    # plt.plot()
+# --- animation
+# path = os.path.join(os.path.dirname(__file__), "_dqn.gif")
+# runner.animation_save_gif(path)
 
-# 可視化
-# runner.render_terminal()
 
-# animation
-# render = runner.animation_save_gif("_SampleEnv.gif", render_scale=3)
+if __name__ == "__main__":
+    main()
